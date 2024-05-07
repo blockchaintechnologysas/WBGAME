d825893d783fdbf3fd7257122be0e5a01a867db8
Blockchain Technology SAS <83485324+blockchaintechnologysas@users.noreply.github.com>
2024-05-07 09:56:55 -0500

    Update 17_Publicistas.sol

--- wallet/17_Publicistas.sol
+++ wallet/17_Publicistas.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Opt 5000 Red Scolcoin (Scol wei Chain)
+// Opt 5000 Red Scolcoin (Scol wei Chain) Direccion: 0x5493023045CCcB9b79A6dF5d691bB29369908534
 //*********************************************************************
 // BLOCKCHAIN TECHNOLOGY SOLUTIONS AND ARTIFICIAL INTELLIGENCE AI S.A.S
 //*********************************************************************
