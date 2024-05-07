07efde4510b4674dc5a366ed5410694e42732b4f
Blockchain Technology SAS <83485324+blockchaintechnologysas@users.noreply.github.com>
2024-05-06 20:12:55 -0500

    Create diseno_grafico.sol

---
+++ wallet/diseno_grafico.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Opt 5000 Red Scolcoin (Scol wei Chain)
+//*********************************************************************
+// BLOCKCHAIN TECHNOLOGY SOLUTIONS AND ARTIFICIAL INTELLIGENCE AI S.A.S
+//*********************************************************************
+//# locked Fondo(0.5%) 12 meses, para Estudiantes Patrocinados para la Carrera
+// Multimedia de WB GAME METAVERSO (Empleados de medio tiempo, se otorga por meritos)
+// Aplica Terminos y Condiciones Cubre solo el semestre Cupo 145 por año.
