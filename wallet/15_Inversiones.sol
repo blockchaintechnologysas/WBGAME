34f79b10566f13645263ceaa7c9cae99537f12ef
Blockchain Technology SAS <83485324+blockchaintechnologysas@users.noreply.github.com>
2024-05-06 20:39:13 -0500

    Create 15_Inversiones.sol

---
+++ wallet/15_Inversiones.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// Opt 5000 Red Scolcoin (Scol wei Chain)
+//*********************************************************************
+// BLOCKCHAIN TECHNOLOGY SOLUTIONS AND ARTIFICIAL INTELLIGENCE AI S.A.S
+//*********************************************************************
+//# Fondo(10%), para Inversiones de WB GAME METAVERSO
+
