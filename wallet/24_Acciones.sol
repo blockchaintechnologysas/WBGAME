a38cd68a7ca1c4aef8f49a3f93490ab7778c8b35
Blockchain Technology SAS <83485324+blockchaintechnologysas@users.noreply.github.com>
2024-05-07 18:51:25 -0500

    Update 24_Acciones.sol

--- wallet/24_Acciones.sol
+++ wallet/24_Acciones.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Opt 5000 Red Scolcoin (Scol wei Chain)
+// Opt 5000 Red Scolcoin (Scol wei Chain) Direccion: 0x92437953db53C3C0D1B9cb0F3B3A8b39A07463b5
 //*********************************************************************
 // BLOCKCHAIN TECHNOLOGY SOLUTIONS AND ARTIFICIAL INTELLIGENCE AI S.A.S
 //*********************************************************************
